cd3846bda983fa9dc9389a6d53339c8637171b1e
𝓥𝓮𝔁𝓣𝓱𝓮𝓟𝓻𝓸𝓽𝓸𝓰𝓮𝓷 <149710902+zv8001@users.noreply.github.com>
2026-03-13 17:39:05 -0500

    Update Form1.vb

--- Bitdefender SecurePass Client/Form1.vb
+++ Bitdefender SecurePass Client/Form1.vb
@@ -4,7 +4,7 @@
 Imports System.Net.NetworkInformation
 Public Class Form1
 
-    Function HasInternet() As Boolean
+    Function HasInternet() As Boolean 'My good friend Chat GPT wrote this function.
         Try
             Dim PingSender As New Ping()
             Dim Reply = PingSender.Send("8.8.8.8", 1000)
