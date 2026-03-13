04888fe2a897757ad2d1525dfdbbb69a300ac98c
𝓥𝓮𝔁𝓣𝓱𝓮𝓟𝓻𝓸𝓽𝓸𝓰𝓮𝓷 <z3ck200@gmail.com>
2026-03-13 17:26:41 -0500

    plz dont kill me

---
+++ Bitdefender SecurePass Client/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.White
+        WebView21.Location = New Point(1, -1)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(909, 508)
+        WebView21.TabIndex = 0
+        WebView21.ZoomFactor = 1R
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(910, 505)
+        Controls.Add(WebView21)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Bitdefender SecurePass Client (not official made by VexTheProtogen)"
+        CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+
+End Class
---
+++ Bitdefender SecurePass Client/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net.NetworkInformation
+Public Class Form1
+
+    Function HasInternet() As Boolean
+        Try
+            Dim PingSender As New Ping()
+            Dim Reply = PingSender.Send("8.8.8.8", 1000)
+
+            If Reply.Status = IPStatus.Success Then
+                Return True
+            End If
+        Catch
+        End Try
+
+        Return False
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not HasInternet() Then
+            MsgBox("Please ensure you have a valid internet connection and try again.", 0 + 16, "ERROR")
+            Me.Close()
+        Else
+            WebView21.Source = New Uri("https://securepass.bitdefender.com/index.html")
+        End If
+    End Sub
+
+    Private Sub WebView21_Click(sender As Object, e As EventArgs) Handles WebView21.Click
+
+    End Sub
+End Class
